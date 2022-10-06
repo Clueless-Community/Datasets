1421c86f0ae0279e87183ddcbf39c96e90f029be
VIJAY 'H'CN <vijaychowdary3699@gmail.com>
2022-10-06 18:34:25 +0530

    update

--- create_datasets_md.py
+++ create_datasets_md.py
@@ -2,27 +2,28 @@
 target: create a python script to automate the datasets.md file updation every time there is change in the data folder files.
 This folder should not be changes unless the related changes are updated in this script.
 Avoid spaces while naming the file/folder
-'''
+# '''
+
+
 import os
 
-data_path = os.path.join(".", "Data")
+data_path = os.path.join(".", "Data") # ./Data
 
-with open("DATASETS.md", "w") as mdFile:
-    mdFile.write(f"# All Datasets\n")
+with open("DATASETS.md", 'w') as mdFile:
+    mdFile.write("## All DATASETS.\n")
     for root, folders, files in os.walk(data_path):
-        baseName = os.path.basename(root)
-        if len(files) > 0:
-            mdFile.write(f"## {baseName.upper()}\n")
+        if len(files)> 0:
+            baseName = os.path.basename(root)
+            mdFile.write(f"### {baseName.upper()}\n")
             for f in files:
+                title, link = '', ''
                 title = f.split(".")[0].upper()
                 link = os.path.join(root, f)
-                # exception case
-                # in windows file path contains \ instead of / but git accepts / as relative path else it gives 404 error
                 while "\\" in link:
                     link = link.replace("\\", "/")
-                content = f"- [{title}]({link})"
+                content = f"[{title}]({link})"
                 mdFile.write(content)
                 mdFile.write("\n")
-        mdFile.write("\n")
-
-mdFile.close()
+            mdFile.write("\n")
+        pass
+        
